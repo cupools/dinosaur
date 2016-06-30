145f3cd2b4c64017ab421d49cd14280efa793695
liyh <lyh106415@alibaba-inc.com>
2016-06-30 21:21:58 +0800

    Official demo test

--- src/createjs.js
+++ src/createjs.js
@@ -1,6 +1,7 @@
 'use strict';
 
-import {EaselJS} from 'imports?this=>window!exports?window.createjs!EaselJS';
-import {TweenJS} from 'imports?this=>window!exports?window.createjs!TweenJS';
+import 'imports?this=>window!exports?window.createjs!EaselJS';
+import 'imports?this=>window!exports?window.createjs!TweenJS';
+import 'imports?this=>window!exports?window.createjs!PreloadJS';
 
 export default window.createjs;
